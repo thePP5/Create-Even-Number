aa921e6ba39607d489bca9230b29f376e214e1cd
Umapathysarma Prathaban <prathaprasan@gmail.com>
2022-12-10 15:24:57 +1100

    created even numbers by pressing button and reset it back

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React,{useState} from 'react';
+
+const CreateEvenNumber=()=> {
+  const[count,setCount] = useState(2)
+  const incrementByEven=()=>{
+    setCount(count*2);
+  }
+
+  const resetAll =()=>{
+    setCount(2)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {count}
+    <button onClick={incrementByEven}>Click Me!</button>
+    <button onClick={resetAll}>Reset</button>
     </div>
   );
 }
 
-export default App;
+export default CreateEvenNumber;
